4fe4f4a556f81ed4a6c3c305af26dbba691acb14
nicolau <marcosnicolau2003@gmail.com>
2024-06-17 15:43:13 -0300

    random pos util

--- 2024-1C-3051-fossers/src/Utils/Vectors.cs
+++ 2024-1C-3051-fossers/src/Utils/Vectors.cs
@@ -3,8 +3,10 @@
 using System;
 using Microsoft.Xna.Framework;
 
-public static class VectorUtils {
-    public static Vector3 GetRandomVec3Pos(Vector3 center, Random _rand = null) {
+public static class VectorUtils
+{
+    public static Vector3 GetRandomVec3Pos(Vector3 center, Random _rand = null)
+    {
         Random rand = _rand ?? new();
         return center + new Vector3(rand.Next(-10000, 10000), 0, rand.Next(-10000, 10000));
     }
