05627bff3db14ea7ab9d9e86c036454bde40edba
nicolau <marcosnicolau2003@gmail.com>
2024-06-17 15:42:39 -0300

    enemy gameobject

---
+++ 2024-1C-3051-fossers/src/GameObjects/GameObjects/Enemy.cs
@@ -0,0 +1,116 @@
+using System;
+using Microsoft.Xna.Framework;
+using WarSteel.Common;
+using WarSteel.Common.Shaders;
+using WarSteel.Entities;
+using WarSteel.Managers;
+using WarSteel.Scenes;
+using WarSteel.Utils;
+
+public class Enemy : GameObject
+{
+    private float _health = 100;
+    public float Health
+    {
+        get => _health;
+        set => _health = value;
+    }
+    private float _damage;
+    private Player _player;
+    private float _chaseRange = 8000f;
+    private float _attackRange = 3000f;
+    private float _speed = 10000f;
+    private bool _canMove = true;
+    private float _delayBeforeMove = 1000f;
+    private float _currentDelay = 0;
+    private DynamicBody _rb;
+    private Transform _turretTransform;
+    private Transform _cannonTransform;
+    public bool _isReloading = false;
+    public int _reloadingTimeInMs = 3000;
+    public float _bulletForce = 36000;
+
+
+    public Enemy(Vector3 pos, float damage, Player player) : base(new string[] { }, new() { Position = pos }, ContentRepoManager.Instance().GetModel("Tanks/Panzer/Panzer"))
+    {
+        _turretTransform = new(Transform, Vector3.Zero);
+        _cannonTransform = new(_turretTransform, Vector3.Zero);
+        _defaultRenderer = new TankRenderable(_turretTransform, _cannonTransform);
+        _damage = damage;
+        _player = player;
+
+        _rb = new DynamicBody(new Collider(new BoxShape(200, 325, 450), (c) => { }), new Vector3(0, 100, 0), 200, 0.9f, 2f);
+        AddComponent(_rb);
+    }
+
+    public override void Update(Scene scene, GameTime gameTime)
+    {
+        base.Update(scene, gameTime);
+
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        Vector3 directionToPlayer = _player.Transform.AbsolutePosition - Transform.AbsolutePosition;
+        float distanceToPlayer = directionToPlayer.Length();
+
+        if (distanceToPlayer > 0)
+        {
+            if (!_canMove)
+            {
+                _currentDelay += deltaTime;
+                if (_currentDelay >= _delayBeforeMove)
+                {
+                    _canMove = true;
+                }
+            }
+
+            directionToPlayer.Normalize();
+
+            if (distanceToPlayer <= _chaseRange && distanceToPlayer >= _attackRange)
+            {
+                _rb.ApplyForce(directionToPlayer * _speed);
+            }
+
+            if (distanceToPlayer <= _attackRange)
+            {
+                RotateTurretAndCannon(directionToPlayer);
+                Shoot(scene);
+            }
+        }
+    }
+
+    public void Shoot(Scene scene)
+    {
+        if (_isReloading) return;
+
+        GameObject bullet = CreateBullet();
+        scene.AddGameObject(bullet);
+
+        // Calculate direction towards the player
+        Vector3 directionToPlayer = _player.Transform.AbsolutePosition - Transform.AbsolutePosition;
+        directionToPlayer.Normalize();
+
+        // Apply force to the bullet in the direction towards the player
+        bullet.GetComponent<DynamicBody>().ApplyForce(-_cannonTransform.Forward * _bulletForce);
+
+        _isReloading = true;
+        Timer.Timeout(_reloadingTimeInMs, () => _isReloading = false);
+    }
+
+    private GameObject CreateBullet()
+    {
+        GameObject bullet = new GameObject(new string[] { "bullet" }, new Transform(), ContentRepoManager.Instance().GetModel("Tanks/Bullet"), new Default(0.9f, 0.1f, Color.Red));
+        bullet.AddComponent(new DynamicBody(new Collider(new SphereShape(10), c =>
+        {
+            if (c.Entity.HasTag("player")) _player.Health -= _damage;
+        }), Vector3.Zero, 5, 0, 0));
+        bullet.AddComponent(new LightComponent(Color.White, Vector3.Zero));
+        bullet.Transform.Position = _cannonTransform.AbsolutePosition + _cannonTransform.Forward * 500 + _cannonTransform.Up * 200;
+        return bullet;
+    }
+
+    private void RotateTurretAndCannon(Vector3 direction)
+    {
+        _turretTransform.Orientation = Quaternion.CreateFromRotationMatrix(Matrix.CreateWorld(Vector3.Zero, direction, Vector3.UnitY));
+        _cannonTransform.Orientation = Quaternion.CreateFromRotationMatrix(Matrix.CreateWorld(Vector3.Zero, direction, Vector3.UnitY));
+    }
+}
