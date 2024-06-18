21d934be2c127a603c629b9e45fc356e58fb73a1
nicolau <marcosnicolau2003@gmail.com>
2024-06-18 08:40:39 -0300

    refactor enemy health bar

--- 2024-1C-3051-fossers/src/GameObjects/GameObjects/Enemy.cs
+++ 2024-1C-3051-fossers/src/GameObjects/GameObjects/Enemy.cs
@@ -2,10 +2,12 @@ using Microsoft.Xna.Framework;
 using WarSteel.Common;
 using WarSteel.Entities;
 using WarSteel.Managers;
-using WarSteel.Scenes;
 
 public class Enemy : GameObject
 {
+    private EnemyHealthBar _healthBar;
+    private WaveProcessor _wave;
+
     public bool isDead = false;
     private float _health = 100;
     public float Health
@@ -14,14 +16,12 @@ public class Enemy : GameObject
         set
         {
             _health = value;
-            _healthBarUI._healthBar.SetHealth(value);
+            _healthBar.SetHealth(value);
             if (_health <= 0 && !isDead) OnDie();
         }
     }
     private float _damage;
     public float Damage { get => _damage; }
-    private EnemyHealthBar _healthBarUI;
-    private WaveProcessor _wave;
 
     public Enemy(Vector3 pos, float damage, WaveProcessor wave) : base(new string[] { "enemy" }, new() { Position = pos }, ContentRepoManager.Instance().GetModel("Tanks/Panzer/Panzer"))
     {
@@ -30,6 +30,7 @@ public class Enemy : GameObject
 
         RigidBody rb = new DynamicBody(new Collider(new BoxShape(200, 325, 450), (c) => { }), new Vector3(0, 100, 0), 200, 0.9f, 2f);
         EnemyAI ai = new(turretTransform, cannonTransform);
+        _healthBar = new();
 
         _defaultRenderer = new TankRenderable(turretTransform, cannonTransform);
         _damage = damage;
@@ -37,29 +38,13 @@ public class Enemy : GameObject
 
         AddComponent(rb);
         AddComponent(ai);
+        AddComponent(_healthBar);
     }
 
-    public override void Update(Scene scene, GameTime time)
-    {
-        base.Update(scene, time);
-        _healthBarUI.CalculateHealthPos();
-    }
-
-    public override void Initialize(Scene scene)
-    {
-        base.Initialize(scene);
-        _healthBarUI = new(scene, this);
-    }
 
     private void OnDie()
     {
         _wave.EnemyDie();
         isDead = true;
-    }
-
-    public override void OnDestroy(Scene scene)
-    {
-        _healthBarUI.Remove();
-        base.OnDestroy(scene);
     }
 }
